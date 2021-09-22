c247dc6690e2e23207f1e996c8907cd0437e2c9d
Ramūnas Mažeikis <r.mazeikis@gmc.lt>
2021-09-22 13:09:48 +0300

    Added a xxd-style hexdump function.
    Signed-off-by: Ramūnas Mažeikis <r.mazeikis@gmc.lt>

--- include/str.h
+++ include/str.h
@@ -13,11 +13,17 @@ static inline char gr_lonib(char ch)
 
 static inline char gr_nib2hex(char nib)
 {
-	return (nib > 10)
+	return (nib >= 10)
 		? nib + 'A' - 10
 		: nib + '0';
 }
 
 void gr_buf2hex(char **str, const char *data, size_t len);
+void gr_hexdump(const char *data,
+		size_t len,
+		char *str,
+		size_t group,
+		size_t groups);
+size_t gr_hexdump_len(size_t data_len, size_t group, size_t groups);
 
 #endif /* LIBGRAMAS_STR_H */
--- str.c
+++ str.c
@@ -18,3 +18,55 @@ void gr_buf2hex(char **str, const char *data, size_t len)
 	}
 	*ptr = '\0';
 }
+
+void gr_hexdump(const char *data,
+		size_t len,
+		char *str,
+		size_t group,
+		size_t groups)
+{
+	size_t i = 0;
+
+	while (i < len) {
+		if (i) {
+			if (!(i % (group * groups))) {
+				*str++ = '\n';
+			} else if (!(i % group)) {
+				*str++ = ' ';
+			}
+		}
+		*str++ = gr_nib2hex(gr_hinib(*data));
+		*str++ = gr_nib2hex(gr_lonib(*data++));
+		i++;
+	}
+	if (len % (group * groups))
+		*str++ = '\n';
+	*str++ = '\0';
+}
+
+size_t gr_hexdump_len(size_t data_len, size_t group, size_t groups)
+{
+	size_t ret = 1;
+	size_t full_lines;
+	size_t line_chars;
+	size_t rem_bytes;
+	size_t rem_groups;
+	size_t rem_grp_bytes;
+
+	if (!group || !groups || !data_len)
+		return 0;
+
+	full_lines = data_len / (group * groups);
+	rem_bytes = data_len % (group * groups);
+
+	line_chars = group * 2 * groups + groups;
+	ret += line_chars * full_lines;
+
+	rem_groups = rem_bytes / group;
+	ret += rem_groups * group * 2 + groups;
+
+	rem_grp_bytes = rem_bytes - rem_groups * group;
+	ret += rem_grp_bytes * 2;
+
+	return ret;
+}
